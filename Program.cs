c150de481d301e3751c2528131035388e7c34ef4
erikperik94 <aggfelt94@hotmail.com>
2021-07-06 10:16:22 +0200

    fix

--- Program.cs
+++ Program.cs
@@ -6,18 +6,7 @@ namespace ConsoleAppFour
     {
         static void Main(string[] args)
         {
-            /*Person person1 = new Person("Erik","Aggfelt");
-            Person person2 = new Person("Göran","Persson");
-            Todo work1 = new Todo(4,"clean up");
-            Todo work2 = new Todo(5,"do the dishes");
-
-            Console.WriteLine(person2.Details());
-            Console.WriteLine(work1.Details());
-            Console.WriteLine(work2.Details());
-
-            PersonSequencer what = new PersonSequencer();
-            Console.WriteLine(what.NextPersonId());
-            */
+            Console.WriteLine("Hello World");
         }
     }
 }
